463bec6977175ad69b8213427ef4b5840230fd65
Chris Shaffer <chris@scoutcorpsllc.com>
2017-09-03 16:34:57 -0400

    ready

--- sites/amazon.js
+++ sites/amazon.js
@@ -4,13 +4,16 @@
 
 const sellerLabelSelector = 'span.a-size-small.a-color-secondary';
 
-$(sellerLabelSelector)
-  .toArray()
-  .filter(x => $(x).html().trim() === 'by')
-  .map(by => {
-    var siblings = $(sellerLabelSelector, $(by).parent())
-      .toArray()
-      .filter(s => s !== by);
+$(document).ready(() => {
 
-    $(siblings).css('border', '1px dashed red');
-  });
+  $(sellerLabelSelector)
+    .toArray()
+    .filter(x => $(x).html().trim() === 'by')
+    .map(by => {
+      var siblings = $(sellerLabelSelector, $(by).parent())
+        .toArray()
+        .filter(s => s !== by);
+
+      $(siblings).css('border', '1px dashed red');
+    });
+});
