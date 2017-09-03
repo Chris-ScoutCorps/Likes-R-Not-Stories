42267beca3ce7a3bf0683c87d37450a2193f8bbd
Chris Shaffer <chris@scoutcorpsllc.com>
2017-09-03 17:25:25 -0400

    enable/disable

--- sites/amazon.js
+++ sites/amazon.js
@@ -5,15 +5,20 @@
 const sellerLabelSelector = 'span.a-size-small.a-color-secondary';
 
 $(document).ready(() => {
+  chrome.storage.sync.get('enable-amazon', function (settings) {
+    if (settings['enable-amazon'] === false)
+      return;
 
-  $(sellerLabelSelector)
-    .toArray()
-    .filter(x => $(x).html().trim() === 'by')
-    .map(by => {
-      var siblings = $(sellerLabelSelector, $(by).parent())
-        .toArray()
-        .filter(s => s !== by);
+    $(sellerLabelSelector)
+      .toArray()
+      .filter(x => $(x).html().trim() === 'by')
+      .map(by => {
+        var siblings = $(sellerLabelSelector, $(by).parent())
+          .toArray()
+          .filter(s => s !== by);
 
-      $(siblings).css('border', '1px dashed red');
-    });
+        $(siblings).css('border', '1px dashed red');
+      });
+
+  });
 });
