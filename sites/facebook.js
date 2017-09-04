81c787132a0d690e2faec001779ff78a7f7de6b9
Chris Shaffer <chris@scoutcorpsllc.com>
2017-09-03 21:46:38 -0400

    aggregation function makes sure we're not running our code every millsecond

--- sites/facebook.js
+++ sites/facebook.js
@@ -3,6 +3,35 @@
 const postContainerSelector = 'div.fbUserPost';
 const postHeadlineSelector = 'h5 .fwn.fcg>.fcg';
 
+var aggregationKeys = {};
+function aggregate(key, func, wait, maxWait) {
+  if (maxWait == null) maxWait = wait * 3;
+
+  function funcWithMark() {
+    func();
+    aggregationKeys[key].last = (new Date()).getTime();
+  }
+
+  const waitedTooLong = aggregationKeys[key]
+    && aggregationKeys[key].last
+    && (aggregationKeys[key].last + maxWait < (new Date()).getTime());
+
+  if (waitedTooLong) {
+    funcWithMark();
+  }
+
+  if (aggregationKeys[key]) {
+    clearTimeout(aggregationKeys[key].id);
+  }
+
+  if (!waitedTooLong) { //because we'd have already ran it
+    if (!aggregationKeys[key])
+      aggregationKeys[key] = { last: null };
+
+    aggregationKeys[key].id = setTimeout(funcWithMark, wait);
+  }
+}
+
 function likesAreNotStories() {
   $(postContainerSelector).toArray().forEach(post => {
     if ($(post).data('likesAreNotStories-visited'))
@@ -30,8 +59,8 @@ $(document).ready(() => {
     if (settings['enable-facebook'] === false)
       return;
 
-    likesAreNotStories();
-    $(document).scroll(likesAreNotStories);
+    aggregate('likesAreNotStories', likesAreNotStories, 100);
+    $(document).scroll(() => aggregate('likesAreNotStories', likesAreNotStories, 100));
 
   });
 
