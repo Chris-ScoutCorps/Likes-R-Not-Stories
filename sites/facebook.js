57beac73e692cbe7803efa31a511ea4ec2dac387
Chris Shaffer <chris@scoutcorpsllc.com>
2017-09-04 14:46:54 -0400

    hide/show

--- sites/facebook.js
+++ sites/facebook.js
@@ -7,6 +7,7 @@ function likesAreNotStories() {
   $(postContainerSelector).toArray().forEach(post => {
     if ($(post).data('likesAreNotStories-visited'))
       return;
+    $(post).data('likesAreNotStories-visited', true);
 
     $(postHeadlineSelector, post[0]).toArray().forEach(headline => {
       const copy = $(headline).clone();
@@ -14,14 +15,22 @@ function likesAreNotStories() {
       $('a', copy[0]).remove();
       const nut = $(copy).html().replace(/\./g, '').trim();
       if (nut === 'liked this' || nut === 'likes' || nut === 'reacted to this' || nut.startsWith('liked this post from')) {
-        const backup = $(headline).clone();
-        $(post).css('padding', '2px');
-        $(post).html('');
-        $(post).append(backup);
+        const newPost = $(post).clone();
+        $(newPost).html('').css('padding', '2px');
+        $(newPost).data('likesAreNotStories-visited', true);
+
+        const newHeadline = $(headline).clone();
+        $(newPost).append(newHeadline);
+
+        const show = $('<a href="javascript:void(0);">(show)</a>');
+        $(newHeadline).append('<span> </span>').append(show);
+        $(show).click(function () { $(post).show(); $(newPost).remove(); });
+
+        $(post).after(newPost);
+        $(post).hide();
       }
     });
 
-    $(post).data('likesAreNotStories-visited', true);
   });
 }
 
