55b8adbd2a124ef2a8de4adccea4384386e775a9
Chris Shaffer <chris@scoutcorpsllc.com>
2017-09-03 21:19:32 -0400

    likes are not stories

--- sites/facebook.js
+++ sites/facebook.js
@@ -1,13 +1,38 @@
 'use strict';
 
-console.log('Facebook ')
+const postContainerSelector = 'div.fbUserPost';
+const postHeadlineSelector = 'h5 .fwn.fcg>.fcg';
+
+function likesAreNotStories() {
+  $(postContainerSelector).toArray().forEach(post => {
+    if ($(post).data('likesAreNotStories-visited'))
+      return;
+
+    $(postHeadlineSelector, post[0]).toArray().forEach(headline => {
+      const copy = $(headline).clone();
+      $('.fwb', copy[0]).remove();
+      $('a', copy[0]).remove();
+      const nut = $(copy).html().replace(/\./g, '').trim();
+      if (nut === 'liked this' || nut === 'likes') {
+        const backup = $(headline).clone();
+        $(post).css('padding', '2px');
+        $(post).html('');
+        $(post).append(backup);
+      }
+    });
+
+    $(post).data('likesAreNotStories-visited', true);
+  });
+}
 
 $(document).ready(() => {
   chrome.storage.sync.get('enable-facebook', function (settings) {
     if (settings['enable-facebook'] === false)
       return;
 
-    console.log(' enabled')
+    likesAreNotStories();
+    $(document).scroll(likesAreNotStories);
+
   });
 
 });
